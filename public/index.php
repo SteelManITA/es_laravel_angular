049de251cd66b1bb3db68f8807b85a2391104b59
SteelManITA <concetto.pollicina@gmail.com>
2017-06-06 12:43:01 +0200

    modificato 'Access-Control-Allow-Headers: *' in 'Content-Type, Accept'

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Headers: Content-Type, Accept");
 header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header('Content-Type: application/json');
 
